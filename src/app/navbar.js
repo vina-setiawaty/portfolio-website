fa96d97844ec7bc8427a574a6b157d5b015f3c9d
vina-setiawaty <vina.vn071@gmail.com>
2025-07-31 23:27:46 +0700

    Intro heading

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -11,22 +11,24 @@ export const Navbar = () => {
     }
 
     return (
-        <nav className="flex items-center justify-between text-lg px-[48px] py-[24px] bg-primary text-accent font-main">
-        <div className="text-lg font-bold">
-            <Image src="/assets/icon.svg" alt="Logo" width={48} height={48} className="h-8 w-8 inline-block" />
-        </div>
-        <ul className="flex space-x-[48px]" style={listStyle}>
-            <li>
-            <Link href="/" className="hover:underline">
-                Projects
-            </Link>
-            </li>
-            <li>
-            <Link href="/about" className="hover:underline">
-                About
-            </Link>
-            </li>
-        </ul>
+        <nav className="fixed w-full text-sm md:text-lg bg-white text-accent font-main">
+            <div className="m-auto flex w-full max-w-[2160px] items-center justify-between  px-[12px] md:px-[48px] py-[12px] md:py-[24px]">
+                <div className="text-lg font-bold">
+                    <Image src="/assets/icon.svg" alt="Logo" width={48} height={48} className="h-6 md:h-8 inline-block" />
+                </div>
+                <ul className="flex space-x-[24px] md:space-x-[48px]" style={listStyle}>
+                    <li>
+                        <Link href="/" className="hover:underline">
+                            Projects
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className="hover:underline">
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
 // import Image from "next/image";
 import { Navbar } from "./navbar";
+import "./index.css";
 
 export default function Home() {
   return (
-    <div>
-    <Navbar />
-    <div className="flex flex-col items-center justify-center min-h-screen bg-main"> 
-
-    </div>
+    <div className="font-secondary bg-primary text-accent flex flex-col min-h-screen items-start justify-start">
+      <Navbar />
+      <div className="flex flex-col items-center justify-start min-h-screen max-w-[2160px] w-full my-[52px] md:my-[80px]">
+        <div className="flex flex-col items-center justify-start min-h-screen max-w-[2160px] w-full my-[48px] gap-4">
+          <h1 className="text-jump text-6xl md:text-8xl text-center font-main font-bold text-accent mb-4">
+            <span>H</span><span>e</span><span>l</span><span>l</span><span>o</span><span>,</span><br />
+            <span>I</span><span>'</span><span>m</span><span>{'\u00A0'}</span><span>V</span><span>i</span><span>n</span><span>a</span>
+          </h1>
+          <h2 className="text-lg md:text-2xl text-center font-secondary text-accent mb-8 mx-[12px] md:mx-[48px] max-w-[1280px]">
+            With a curious mind and a passion for design, I aim to blend technology into daily life in ways that feel natural and enriching.
+          </h2>
+          <hr className="h-[1px] w-[80%] px-[12px] md:px-[48px] xl:max-w-[1280px]" />
+        </div>
+      </div>
     </div>
   );
 }

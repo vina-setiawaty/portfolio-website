55225ce29319ba9897764fd8b99f3f7e9a4527ab
vina-setiawaty <vina.vn071@gmail.com>
2025-08-01 17:28:07 +0700

    Updated apostrophe according to ESLint error

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <div className="font-secondary bg-primary text-accent flex flex-col min-h-screen items-start justify-start">
       <Navbar />
-      <div className="flex flex-col items-center justify-start max-w-[2160px] w-full my-[52px] md:my-[80px]">
+      <div className="flex flex-col m-auto items-center justify-start max-w-[2160px] w-full my-[52px] md:my-[80px]">
         <div className="flex flex-col items-center justify-start max-w-[2160px] w-full my-[48px] gap-4 md:gap-8">
           <h1 className="text-jump text-6xl md:text-8xl text-center font-main font-bold text-accent mb-4">
             <span>H</span><span>e</span><span>l</span><span>l</span><span>o</span><span>,</span><br />
-            <span>I</span><span>'</span><span>m</span><span>{'\u00A0'}</span><span>V</span><span>i</span><span>n</span><span>a</span>
+            <span>I</span><span>&apos;</span><span>m</span><span>{'\u00A0'}</span><span>V</span><span>i</span><span>n</span><span>a</span>
           </h1>
           <h2 className="text-lg md:text-2xl text-center font-main text-accent mb-8 mx-[12px] md:mx-[48px] max-w-[1280px]">
             With a curious mind and a passion for design, I aim to blend technology into daily life in ways that feel natural and enriching.

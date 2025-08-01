5ce2fad0cd8701ffbbff5b302524c483d7feef41
vina-setiawaty <vina.vn071@gmail.com>
2025-08-01 19:20:34 +0700

    addition of homepage in package.json

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const isGithubPages = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  basePath: "/portfolio-website",
+//   basePath: "/portfolio-website",
   output: "export",
   reactStrictMode: true,
 };

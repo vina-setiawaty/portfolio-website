09db4174788ef37065a3e2de70517db5fd81aeda
vina-setiawaty <vina.vn071@gmail.com>
2025-08-01 18:02:20 +0700

    updating config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const isGithubPages = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+    output: 'export', // required for static export
     basePath: isGithubPages ? '/portfolio-website' : '',
     assetPrefix: isGithubPages ? '/portfolio-website/public/' : '',
 };
